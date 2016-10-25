d81e3d84cef732cbd250e9d6385f44606b11d6ea
Alicia Beltran <aliceresponde@gmail.com>
2016-10-25 00:33:50 -0500

    Working on  frame animation

--- app/src/main/java/com/example/alice/animation_graphics/MainActivity.java
+++ app/src/main/java/com/example/alice/animation_graphics/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.alice.animation_graphics;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 /**
  * Frame by frame animation
@@ -10,7 +14,34 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        final ImageView imgRunner =(ImageView) findViewById(R.id.imgVRunner);
+//        imgRunner.setBackgroundResource(R.drawable.my_frame_animation);
+//        imgRunner.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Log.i("runner", "click");
+//                AnimationDrawable animationDrawable = (AnimationDrawable) imgRunner.getBackground();
+//                if (animationDrawable!=null) {
+//                    animationDrawable.start();
+//                }else
+//                    Log.e("runner","error");
+//            }
+//        });
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ImageView iv = (ImageView) findViewById(R.id.imgVRunner);
+        iv.setBackgroundResource(R.drawable.my_frame_animation);
+        iv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AnimationDrawable anim = (AnimationDrawable) iv.getBackground();
+                anim.start();
+            }
+        });
     }
 }
