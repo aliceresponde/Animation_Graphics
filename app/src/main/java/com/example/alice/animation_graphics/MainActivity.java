1a75d00034dee7343454e58ada718170b24694ab
Alicia Beltran <aliceresponde@gmail.com>
2016-10-24 23:42:54 -0500

    Create main project structure an drawables for runner  (frame by frame)

--- app/src/main/java/com/example/alice/animation_graphics/MainActivity.java
+++ app/src/main/java/com/example/alice/animation_graphics/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.alice.animation_graphics;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * Frame by frame animation
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
