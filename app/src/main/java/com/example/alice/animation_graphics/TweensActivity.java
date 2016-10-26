c4c2bbbac3d1d3e800eafbf1828f97d705466ac9
Alicia <aliceresponde@gmail.com>
2016-10-26 12:31:12 -0500

    Twieens_animation Button duplicate size, back to original size, change transparency, and rotate 360 degres.

--- app/src/main/java/com/example/alice/animation_graphics/TweensActivity.java
+++ app/src/main/java/com/example/alice/animation_graphics/TweensActivity.java
@@ -2,6 +2,9 @@ package com.example.alice.animation_graphics;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 public class TweensActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class TweensActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tweens);
+    }
+
+    public void animateMe(View view) {
+        Animation animation = AnimationUtils.loadAnimation(this,R.anim.tweens_animation);
+        view.startAnimation(animation);
     }
 }
